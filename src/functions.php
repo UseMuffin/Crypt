03df06fec8eafeebf07ecbf15052e9bc08810148
jadb <jadbitar@mac.com>
2016-01-16 13:46:30 -0500

    Fix oracle padding attacks vulnerability, closes #1

--- src/functions.php
+++ src/functions.php
@@ -13,7 +13,7 @@ if (!function_exists('\Muffin\Crypt\public_encrypt')) {
      */
     function public_encrypt($plain, $certificate)
     {
-        openssl_public_encrypt($plain, $cipher, openssl_get_publickey($certificate));
+        openssl_public_encrypt($plain, $cipher, openssl_get_publickey($certificate), OPENSSL_PKCS1_OAEP_PADDING);
         return base64_encode($cipher);
     }
 }
@@ -31,7 +31,7 @@ if (!function_exists('\Muffin\Crypt\private_decrypt')) {
     function private_decrypt($cipher, $certificate, $passphrase = null)
     {
         $resource = openssl_get_privatekey($certificate, $passphrase);
-        openssl_private_decrypt(base64_decode($cipher), $plain, $resource);
+        openssl_private_decrypt(base64_decode($cipher), $plain, $resource, OPENSSL_PKCS1_OAEP_PADDING);
         return $plain;
     }
 }
